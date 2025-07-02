d9e36ba4cba0bacfbd29b615fdff1bd5a973c998
Timi007 <timi007gp@gmail.com>
2025-07-02 21:28:22 +0200

    Fix main dependency

--- extras/blank_config/config.cpp
+++ extras/blank_config/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"PzGrenBtl402_Main"};
+        requiredAddons[] = {"pzgrenbtl402_jsrs2025_main"};
         author = ECSTRING(Main,authors);
         authors[] = {""};
         url = ECSTRING(Main,URL);
