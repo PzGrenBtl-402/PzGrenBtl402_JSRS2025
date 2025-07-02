37ae3348af2a3e8717ce69b84f7f160ba25b50b4
Timi007 <timi007gp@gmail.com>
2025-07-02 21:28:31 +0200

    Fix dependency casing

--- addons/redd/config.cpp
+++ addons/redd/config.cpp
@@ -9,11 +9,11 @@ class CfgPatches {
         requiredAddons[] = {
             "pzgrenbtl402_jsrs2025_main",
             "PzGrenBtl402_Redd_Main",
-            "redd_marder_1a5",
+            "Redd_Marder_1A5",
             "Redd_Tank_Gepard_1A2",
             "Redd_Tank_Wiesel_1A4_MK20",
             "Redd_Tank_Wiesel_1A2_TOW",
-            "redd_tank_fuchs_1a4",
+            "Redd_Tank_Fuchs_1A4",
             "rnt_sppz_2a2_luchs",
             "Redd_Tank_LKW_leicht_gl",
             "rnt_lkw_10t_mil_gl_kat_i",
